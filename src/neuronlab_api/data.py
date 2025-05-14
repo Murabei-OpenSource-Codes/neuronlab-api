bd786560ad7e128055f085769973932314b94c88
Pedro Rosário <pedro.rosario@murabei.com>
2025-05-14 11:29:02 -0300

    change formatting and add proxy

--- src/neuronlab_api/data.py
+++ src/neuronlab_api/data.py
@@ -1,5 +1,6 @@
 """NeuronLab Python API."""
 
+import os
 from typing import List
 import requests
 from requests.adapters import HTTPAdapter
@@ -9,12 +10,13 @@ from neuronlab_api.exceptions import (
     NeuronLabInternalServerException,
     NeuronLabBadRequestException,
     NeuronLabUserNotFoundException,
-    NeuronLabInvalidArgs,
-)
+    NeuronLabInvalidArgs)
 
 
 class NeuronLabAPI:
-    def __init__(self, neuronlab_auth_token: str, url: str, max_tries: int = 5):
+    def __init__(
+            self, neuronlab_auth_token: str, url: str,
+            max_tries: int = 5, proxy: str = None):
         """__init__.
 
         Args:
@@ -22,19 +24,22 @@ class NeuronLabAPI:
             url (str): Neuron Lab API URL.
             max_tries (int): Limit of fetch attempts.
         """
-        self.__neuronlab_auth_token = neuronlab_auth_token
-        self.__url = url
+        self._neuronlab_auth_token = neuronlab_auth_token
+        self._url = url
 
         # Mount session
         HTTP_BACKOFF_FACTOR = 0.2
         retry_strategy = Retry(
-            total=max_tries,
-            backoff_factor=HTTP_BACKOFF_FACTOR,
-            raise_on_status=False,
-        )
+            total=max_tries, backoff_factor=HTTP_BACKOFF_FACTOR,
+            raise_on_status=False)
 
-        self.__session = requests.Session()
-        self.__session.mount(self.__url, HTTPAdapter(max_retries=retry_strategy))
+        if proxy is None:
+            proxy = os.getenv("SERASA_API_PROXY")
+        self._proxy = proxy
+
+        self._session = requests.Session()
+        self._session.mount(self._url, HTTPAdapter(max_retries=retry_strategy))
+        self._session.proxies = {"http": self._proxy, "https": self._proxy}
 
     def get_cnpj_dataset(self, cnpjs: List[str]):
         """Call Neuron Lab API to fetch data for a list of CNPJ.
@@ -51,7 +56,7 @@ class NeuronLabAPI:
             NeuronLabAPIException
         """
 
-        return self.__get_document_dataset("cnpj", cnpjs)
+        return self._get_document_dataset("cnpj", cnpjs)
 
     def get_cpf_dataset(self, cpfs: List[str]):
         """Call Neuron Lab API to fetch data for a list of CPF.
@@ -68,9 +73,9 @@ class NeuronLabAPI:
             NeuronLabAPIException
         """
 
-        return self.__get_document_dataset("cpf", cpfs)
+        return self._get_document_dataset("cpf", cpfs)
 
-    def __get_document_dataset(self, document_type: str, documents: List[str]) -> dict:
+    def _get_document_dataset(self, document_type: str, documents: List[str]) -> dict:
         """Call Neuron Lab API to fetch dataset for a list of document data.
 
         Args:
@@ -90,12 +95,9 @@ class NeuronLabAPI:
 
         if len(documents) == 1:
             raise NeuronLabInvalidArgs(
-                message="Empty document list, at least one document expected"
-            )
+                message="Empty document list, at least one document expected")
 
-        headers = {
-            "Authorization": f"Bearer {self.__neuronlab_auth_token}",
-        }
+        headers = {"Authorization": f"Bearer {self._neuronlab_auth_token}"}
 
         comma_separated_documents = ",".join(documents)
         payload = {}
@@ -103,11 +105,9 @@ class NeuronLabAPI:
 
         try:
             document_endpoint = "{}/api/{}/search-{}s".format(
-                self.__url, document_type, document_type
-            )
-            response = self.__session.get(
-                document_endpoint, headers=headers, params=payload
-            )
+                self._url, document_type, document_type)
+            response = self._session.get(
+                document_endpoint, headers=headers, params=payload)
             response.raise_for_status()
             response_json = response.json()
 
@@ -123,13 +123,10 @@ class NeuronLabAPI:
 
             if response.status_code == 400:
                 raise NeuronLabBadRequestException(
-                    message=response_message, payload=payload
-                )
+                    message=response_message, payload=payload)
             elif response.status_code == 403:
                 raise NeuronLabUserNotFoundException(
-                    message=response_message, payload=payload
-                )
+                    message=response_message, payload=payload)
             else:
                 raise NeuronLabInternalServerException(
-                    message=response_message, payload=payload
-                )
+                    message=response_message, payload=payload)
--- src/neuronlab_api/tests/test_neuronlab_api.py
+++ src/neuronlab_api/tests/test_neuronlab_api.py
@@ -1,18 +1,13 @@
 """Test Neuron Lab API."""
 
 import unittest
-import configparser
 import os
 from neuronlab_api.data import NeuronLabAPI
 
-config = configparser.ConfigParser()
-dir_path = os.path.dirname(os.path.realpath(__file__))
-config.read("{}/test_parameters.ini".format(dir_path))
-
-NEURONLAB_AUTH_TOKEN = config["DEFAULT"]["NEURONLAB_AUTH_TOKEN"].strip()
-NEURONLAB_URL = config["DEFAULT"]["NEURONLAB_URL"].strip()
-CNPJ_TEST_INPUT = config["DEFAULT"]["CNPJ_TEST_INPUT"].strip().split(",")
-CPF_TEST_INPUT = config["DEFAULT"]["CPF_TEST_INPUT"].strip().split(",")
+NEURONLAB_AUTH_TOKEN = os.getenv("NEURONLAB_AUTH_TOKEN")
+NEURONLAB_URL = os.getenv("NEURONLAB_URL")
+CNPJ_TEST_INPUT = os.getenv("CNPJ_TEST_INPUT").split(",")
+CPF_TEST_INPUT = os.getenv("CPF_TEST_INPUT").split(",")
 
 
 class TestNeuronLabAPI(unittest.TestCase):
@@ -21,8 +16,7 @@ class TestNeuronLabAPI(unittest.TestCase):
     def test__fetch_cnpj_ok(self):
         neuronlab_api = NeuronLabAPI(
             neuronlab_auth_token=NEURONLAB_AUTH_TOKEN,
-            url=NEURONLAB_URL,
-        )
+            url=NEURONLAB_URL)
 
         response = neuronlab_api.get_cnpj_dataset(cnpjs=CNPJ_TEST_INPUT)
         self.assertEqual(response["totalCNPJs"], len(CNPJ_TEST_INPUT))
@@ -30,8 +24,7 @@ class TestNeuronLabAPI(unittest.TestCase):
     def test__fetch_cpf_ok(self):
         neuronlab_api = NeuronLabAPI(
             neuronlab_auth_token=NEURONLAB_AUTH_TOKEN,
-            url=NEURONLAB_URL,
-        )
+            url=NEURONLAB_URL)
 
         response = neuronlab_api.get_cpf_dataset(cpfs=CNPJ_TEST_INPUT)
         self.assertEqual(response["totalCPFs"], len(CNPJ_TEST_INPUT))
