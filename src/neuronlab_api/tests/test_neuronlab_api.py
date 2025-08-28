d614a5fe1c0d75478b6ce86fbf339c0ce7475c79
Pedro Rosário <pedro.rosario@murabei.com>
2025-08-28 11:41:04 -0300

    feat(formatted documents): add methods to get formatted documents

--- src/neuronlab_api/tests/test_neuronlab_api.py
+++ src/neuronlab_api/tests/test_neuronlab_api.py
@@ -2,6 +2,7 @@
 
 import unittest
 import os
+from datetime import date
 from neuronlab_api.data import NeuronLabAPI
 
 NEURONLAB_AUTH_TOKEN = os.getenv("NEURONLAB_AUTH_TOKEN")
@@ -13,44 +14,29 @@ CPF_TEST_INPUT = os.getenv("CPF_TEST_INPUT").split(",")
 class TestNeuronLabAPI(unittest.TestCase):
     """Test Neuron Lab api."""
 
-    def test__fetch_cnpj_ok(self):
-        neuronlab_api = NeuronLabAPI(
+    def setUp(self):
+        self.neuronlab_api = NeuronLabAPI(
             neuronlab_auth_token=NEURONLAB_AUTH_TOKEN,
             url=NEURONLAB_URL)
 
-        response = neuronlab_api.get_cnpj_dataset(cnpjs=CNPJ_TEST_INPUT)
+    def test__fetch_cnpj_ok(self):
+        response = self.neuronlab_api.get_cnpj_dataset(cnpjs=CNPJ_TEST_INPUT)
         self.assertEqual(response["totalCNPJs"], len(CNPJ_TEST_INPUT))
 
     def test__fetch_cpf_ok(self):
-        neuronlab_api = NeuronLabAPI(
-            neuronlab_auth_token=NEURONLAB_AUTH_TOKEN,
-            url=NEURONLAB_URL)
-
-        response = neuronlab_api.get_cpf_dataset(cpfs=CNPJ_TEST_INPUT)
+        response = self.neuronlab_api.get_cpf_dataset(cpfs=CNPJ_TEST_INPUT)
         self.assertEqual(response["totalCPFs"], len(CNPJ_TEST_INPUT))
 
     def test__fetch_one_cnpj_ok(self):
-        neuronlab_api = NeuronLabAPI(
-            neuronlab_auth_token=NEURONLAB_AUTH_TOKEN,
-            url=NEURONLAB_URL)
-
-        response = neuronlab_api.get_cnpj_dataset(cnpjs=CNPJ_TEST_INPUT[:1])
+        response = self.neuronlab_api.get_cnpj_dataset(cnpjs=CNPJ_TEST_INPUT[:1])
         self.assertEqual(response["totalCNPJs"], 1)
 
     def test__fetch_one_cpf_ok(self):
-        neuronlab_api = NeuronLabAPI(
-            neuronlab_auth_token=NEURONLAB_AUTH_TOKEN,
-            url=NEURONLAB_URL)
-
-        response = neuronlab_api.get_cpf_dataset(cpfs=CNPJ_TEST_INPUT[:1])
+        response = self.neuronlab_api.get_cpf_dataset(cpfs=CNPJ_TEST_INPUT[:1])
         self.assertEqual(response["totalCPFs"], 1)
 
     def test__fetch_one_cnpj_with_score(self):
-        neuronlab_api = NeuronLabAPI(
-            neuronlab_auth_token=NEURONLAB_AUTH_TOKEN,
-            url=NEURONLAB_URL)
-
-        response = neuronlab_api\
+        response = self.neuronlab_api\
                     .with_score()\
                     .get_cnpj_dataset(cnpjs=CNPJ_TEST_INPUT[:1])
         self.assertEqual(response["totalCNPJs"], 1)
@@ -61,19 +47,8 @@ class TestNeuronLabAPI(unittest.TestCase):
         negative_data = cnpj.get("negative_data")
         self.assertIsNotNone(negative_data)
 
-        pefin = negative_data.get("pefin")
-        self.assertIsNotNone(pefin)
-        pefin_summary = pefin.get("summary")
-        self.assertIsNotNone(pefin_summary)
-        self.assertEqual(pefin_summary.get("balance"), 3124.56)
-        self.assertEqual(pefin_summary.get("count"), 2)
-
     def test__fetch_one_cpf_with_score(self):
-        neuronlab_api = NeuronLabAPI(
-            neuronlab_auth_token=NEURONLAB_AUTH_TOKEN,
-            url=NEURONLAB_URL)
-
-        response = neuronlab_api\
+        response = self.neuronlab_api\
                     .with_score()\
                     .get_cpf_dataset(cpfs=CPF_TEST_INPUT)
 
@@ -87,3 +62,13 @@ class TestNeuronLabAPI(unittest.TestCase):
         self.assertIsNotNone(refin_summary)
         self.assertEqual(refin_summary.get("balance"), 324.22)
         self.assertEqual(refin_summary.get("count"), 1)
+
+    def test__fetch_all_cnpj_formatted(self):
+        response = self.neuronlab_api.fetch_all_cnpj_formatted(
+            start_date=date(2025, 8, 26), end_date=date(2025, 8, 28))
+        self.assertIsInstance(response, list)
+
+    def test__fetch_all_cpf_formatted(self):
+        response = self.neuronlab_api.fetch_all_cpf_formatted(
+            start_date=date(2025, 8, 26), end_date=date(2025, 8, 28))
+        self.assertIsInstance(response, list)
